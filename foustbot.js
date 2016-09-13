efa3d3c2c027c16f3bbe3ea4b0e108500fee4a6d
brytree <brycebortree@gmail.com>
2016-09-12 22:07:43 -0700

    first tweet

--- foustbot.js
+++ foustbot.js
@@ -1,5 +1,8 @@
 // "And the ghosts, they own everything."
-var Twit = require('twit');
+var Twit        = require('twit');
+var async       = require('async');
+var wordFilter  = require('wordfilter');
+var env         = require('dotenv').config();
 
 var t = new Twit({
   consumer_key: env.CONSUMER_KEY,
@@ -10,9 +13,46 @@ var t = new Twit({
 
 var wordnikKey = env.WORDNIK_API_KEY
 
-var finTweet = '';
+run = function() {
+  async.waterfall([
+    formatTweet,
+    postTweet
+  ],
+  function(err, botData) {
+    if (err) {
+      console.log('There was an error posting to Twitter: ', err);
+    } else {
+      console.log('Tweet successful!');
+      console.log('Tweet: ', botData.finalTweet);
+    }
+  });
+}
 
-var conjunctions = ["For", "And", "Nor", "But", "Or", "Yet", "So"];
 
-var final = ["something", "anything", "nothing", "everything"];
+formatTweet = function(cb){
+  var botData = {
+    finalTweet: ""
+  }
+
+  var conjunctions = ["For", "And", "Nor", "But", "Or", "Yet", "So"];
+  var finals = ["something", "anything", "nothing", "everything"];
+
+  var conjunction = conjunctions[Math.floor(Math.random()*conjunctions.length)];
+  var final = finals[Math.floor(Math.random()*finals.length)];
+
+  botData.finalTweet = conjunction + " the ghosts, they own " + final + ".";
+  console.log(botData);
+
+  cb(null, botData);
+}
+
+postTweet = function(botData, cb) {
+  t.post('statuses/update', {status: botData.finalTweet}, function(err, data, response) {
+    cb(err, botData);
+  });
+}
+
+run();
+
+
 
