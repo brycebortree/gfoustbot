f2067b5ea358d2f05ca5a619bb37af5c52a39814
brytree <brycebortree@gmail.com>
2016-09-13 21:53:45 -0700

    correcting proper nouns

--- foustbot.js
+++ foustbot.js
@@ -40,7 +40,8 @@ getNoun = function(cb){
     finalTweet: ""
   };
   var client = new Client();
-  var wordnikRandomURL = 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=noun-plural' +
+  var wordnikRandomURL = 'http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=false' +
+      '&includePartOfSpeech=noun-plural&excludePartOfSpeech=proper-noun-plural' +
       '&minCorpusCount=0&maxCorpusCount=-1' +
       '&minDictionaryCount=1&maxDictionaryCount=-1&' +
       'minLength=5&maxLength=-1&limit=10&api_key=';
